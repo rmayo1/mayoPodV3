646581fe108b15e1e7ff94130d6cb67b4e7ba4e7
Grayson Wilkins <gwilkin1@wm320-a229573.campus.ithaca.lan>
2015-02-25 21:46:44 -0500

    SecondViewController
    Added the first sort printing functionality.

--- mayoPod_v2/SecondViewController.swift
+++ mayoPod_v2/SecondViewController.swift
@@ -15,25 +15,47 @@ class SecondViewController: UIViewController {
     ///////////////////// Singleton Code ///////////////////////////
     
     var theSongsModel: sharedSongModel = sharedSongModel.theSharedSongModel
-
     
     ////////////////////////////////////////////////////////////////
     
+    override func viewDidLoad() {
+        //sortSegmenter.selectedSegmentIndex == 0
+        super.viewDidLoad()
+    }
+    
     @IBAction func sortSegment(sender: UISegmentedControl) {
-        /*if sortSegmenter.selectedSegmentIndex==0{
-            // var artList = songList.sorted{$0.name < $1.name}
+        if sortSegmenter.selectedSegmentIndex==0{
+            var songList = theSongsModel.songList.sorted{$0.name < $1.name}
+            var toText: String = ""
+            for song in songList {
+                toText += (song.name + " by: " + song.artist + "\n")
+            }
+            sortedTextView.text = toText
         } else if sortSegmenter.selectedSegmentIndex==1{
-            // var artList = songList.sorted{$0.Artist < $1.Artist}
+            var artistList = theSongsModel.songList.sorted{$0.artist < $1.artist}
+            for song in artistList {
+                
+            }
+            //sortedTextView.text = artList
         } else if sortSegmenter.selectedSegmentIndex==2{
-            // var artList = albumList.sorted{$0.Artist < $1.Artist}
-        }*/
-        sortedTextView.text = theSongsModel.songList[0].getSongName()
+            var albumList = theSongsModel.albumList.sorted{$0.name < $1.name}
+            for album in albumList {
+                
+            }
+            //sortedTextView.text = artList
+        } else if sortSegmenter.selectedSegmentIndex==3{
+            var playlistList = theSongsModel.playlistList.sorted{$0.plistName < $1.plistName}
+            for playlist in playlistList {
+                
+            }
+            //sortedTextView.text = artList
+        }
+        //sortedTextView.text = theSongsModel.songList[0].getSongName()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
 }
